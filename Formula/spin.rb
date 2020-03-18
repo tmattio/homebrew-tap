eee5ce33cfa9c5224bfc1293478ee988f13c6596
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-03-17 22:57:15 -0400

    Release spin 0.5.1

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,10 +1,10 @@
 class Spin < Formula
-  VERSION = '0.5.0'
+  VERSION = '0.5.1'
   desc 'Project scaffolding tool and set of templates for Reason and OCaml.'
   homepage 'https://github.com/tmattio/spin'
   url "https://github.com/tmattio/spin/releases/download/#{VERSION}/spin-darwin-x64.zip"
   version VERSION
-  sha256 'b3d0deef6724718fb40c5b42bf7a3744c2a4d9281c7463e8c7e57066981417e6'
+  sha256 'e5f4db3cbb6bdedf5e52994024b5b8cfccdec8548939571d265fb36cb8082410'
 
   bottle :unneeded
 
